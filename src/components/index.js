4834ff67b52080e558feb61149588c49a371039a
sajidCode <muhammadsajidkhan616@gmail.com>
2022-12-07 07:32:38 +0500

    adding functionalilty

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,26 @@
+let words = require("an-array-of-english-words");
+
 class Car {
-    constructor(words) {
+    constructor(words, player, words1) {
         this.words = words;
+        this.player = player;
+        this.words1 = words1;
+    }
+
+    randamizedWords() {
+        return this.words.sort(() => Math.random() - 0.5);
+    }
+
+    RenderWords() {
+        // let randomWords = this.randamizedWords()
+        // for(let i = 0; i < randomWords.length; i++) {
+        //     if(randomWords[i].length <= 3) {
+        //     }
+        // }
     }
 }
+
+let player1 = document.querySelector(".car1");
+let words1 = document.querySelector(".words1");
+
+let car1 = new Car(words, player1, words1);
